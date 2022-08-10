534b2f71905266b9bf3aad8eff10d6de4e0b4928
Lucas Duarte <lukazds2016@gmail.com>
2022-08-09 22:11:57 -0300

    Finishing signin

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -2,7 +2,9 @@ import bcrypt from 'bcrypt';
 import connection from "../dbStrategy/postgres.js";
 
 async function getUserByEmail(email) {
-  return connection.query(`SELECT * FROM users WHERE email = $1 `, [email]);
+
+  return connection.query(`SELECT * FROM users WHERE email = $1`, [email]);
+
 };
 
 async function createUser(email, username, password, photo) {
@@ -10,12 +12,16 @@ async function createUser(email, username, password, photo) {
   const passwordHash = bcrypt.hashSync(password, SALT);
   return await connection.query(`
     INSERT INTO users (email, username, password, photo) 
-    VALUES ($1, $2, $3, $4)`, 
+    VALUES ($1, $2, $3, $4)`,
     [email, username, passwordHash, photo]);
 };
 
-async function getUserByUsername(username){
-  return connection.query(`SELECT email FROM users WHERE username =($1)`, [username]);
+async function getUserByUsername(username) {
+
+  const query = 'SELECT email FROM users WHERE username = $1';
+
+  return connection.query(query, [username]);
+
 };
 
 const usersRepository = {
