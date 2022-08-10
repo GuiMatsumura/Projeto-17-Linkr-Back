c50e160603454ff53471e2b78c63622a9a56cad5
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-08-10 19:49:17 -0300

    add timestamp to posts table

--- linkr_postgres_create.sql
+++ linkr_postgres_create.sql
@@ -8,21 +8,22 @@ CREATE TABLE "users" (
 ) WITH (
   OIDS=FALSE
 );
-
-
-
+​
+​
+​
 CREATE TABLE "posts" (
 	"id" serial NOT NULL,
 	"userId" int NOT NULL,
 	"url" TEXT NOT NULL,
 	"description" TEXT,
+	"createdAt" TIMESTAMP NOT NULL,
 	CONSTRAINT "posts_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
+​
+​
+​
 CREATE TABLE "hashtags" (
 	"id" serial NOT NULL,
 	"name" varchar(100) NOT NULL,
@@ -30,9 +31,9 @@ CREATE TABLE "hashtags" (
 ) WITH (
   OIDS=FALSE
 );
-
-
-
+​
+​
+​
 CREATE TABLE "likes" (
 	"id" serial NOT NULL,
 	"userId" int NOT NULL,
@@ -41,9 +42,9 @@ CREATE TABLE "likes" (
 ) WITH (
   OIDS=FALSE
 );
-
-
-
+​
+​
+​
 CREATE TABLE "hashtagsPost" (
 	"id" serial NOT NULL,
 	"hashtagId" int NOT NULL,
@@ -52,21 +53,18 @@ CREATE TABLE "hashtagsPost" (
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-
+​
+​
+​
+​
 ALTER TABLE "posts" ADD CONSTRAINT "posts_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
-
-
+​
+​
 ALTER TABLE "likes" ADD CONSTRAINT "likes_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
 ALTER TABLE "likes" ADD CONSTRAINT "likes_fk1" FOREIGN KEY ("postId") REFERENCES "posts"("id");
-
+​
 ALTER TABLE "hashtagsPost" ADD CONSTRAINT "hashtagsPost_fk0" FOREIGN KEY ("hashtagId") REFERENCES "hashtags"("id");
 ALTER TABLE "hashtagsPost" ADD CONSTRAINT "hashtagsPost_fk1" FOREIGN KEY ("postId") REFERENCES "posts"("id");
-
-
-
-
-
-
+​
+​
+​
