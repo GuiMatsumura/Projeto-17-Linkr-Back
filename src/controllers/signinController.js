17ec15740f71d0d90c856893f4215b48ca023faf
Lucas Duarte <lukazds2016@gmail.com>
2022-08-09 21:16:16 -0300

    creating schema to validate login

---
+++ src/controllers/signinController.js
@@ -0,0 +1,17 @@
+import jwt from "jw"
+
+export async function userSignin(req, res) {
+
+    try {
+
+        //const token = jwt.sign(req.body.email, process.env.JWT_SECRET);
+
+        res.sendStatus(200);
+
+    } catch (error) {
+
+        res.sendStatus(500);
+        console.error(error);
+
+    }
+};
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { userSignup } from '../controllers/signupController.js';
+import { userSignin } from '../controllers/signinController.js';
 import { validadeSignup } from '../middlewares/validateSignup.js';
 
 const router = express.Router();
 
-router.post("/signup", validadeSignup, userSignup)
+router.post("/signup", validadeSignup, userSignup);
+router.post("/signin", userSignin);
 
 export default router;
