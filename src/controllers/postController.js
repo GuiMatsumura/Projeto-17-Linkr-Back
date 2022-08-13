90c5de6765acd4bcd0b994b40dacf21464d2e4ca
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-08-13 19:39:52 -0300

    create add hashtag freature

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,4 @@
-import postRepository from "../repositories/postRepository.js";
+import postRepository from '../repositories/postRepository.js';
 export async function newPost(req, res) {
   const { verified } = res.locals;
   const body = {
@@ -7,6 +7,14 @@ export async function newPost(req, res) {
   };
   try {
     await postRepository.createPost(body);
+    const searchHashtag = await postRepository.searchHashtag(body);
+    const findHashtag = await postRepository.findHashtag(searchHashtag.rows);
+    if (findHashtag.rows.length === 0) {
+      await postRepository.createPost(body);
+      res.status(201).send({ ...body, userPhoto: verified.photo });
+      return;
+    }
+    await postRepository.insertHashtag(findHashtag.rows);
     res.status(201).send({ ...body, userPhoto: verified.photo });
   } catch (error) {
     console.log(error);
--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -1,4 +1,4 @@
-import connection from "../dbStrategy/postgres.js";
+import connection from '../dbStrategy/postgres.js';
 
 function createPost(body) {
   return connection.query(
@@ -7,4 +7,23 @@ function createPost(body) {
   );
 }
 
-export default { createPost };
+function searchHashtag(body) {
+  return connection.query(
+    `SELECT id AS "postId", REGEXP_MATCHES('$1', '#([A-Za-z0-9_]+)', 'g') AS hashtag FROM posts WHERE posts.description = $2`,
+    [body.description, body.description]
+  );
+}
+
+function findHashtag(hashtag) {
+  return connection.query(`SELECT * FROM hashtags WHERE name = $1`, [
+    hashtag[0].hashtag,
+  ]);
+}
+
+function insertHashtag(hashtag) {
+  return connection.query(`INSERT INTO hashtags (name) VALUES ($1)`, [
+    hashtag[0].name,
+  ]);
+}
+
+export default { createPost, searchHashtag, findHashtag, insertHashtag };
