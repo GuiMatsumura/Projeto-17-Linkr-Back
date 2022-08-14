f9e099bfd1d8868e25aa0c0b82abb55fcff33c09
thaisealves <alves.thaisesilva@gmail.com>
2022-08-14 17:51:50 -0300

    feat: changing some repositories

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -7,16 +7,28 @@ export async function newPost(req, res) {
   };
   try {
     await postRepository.createPost(body);
-    const searchHashtag = await postRepository.searchHashtag(body);
-    const findHashtag = await postRepository.findHashtag(searchHashtag.rows);
-
-    if (findHashtag.rows.length === 0) {
+    const { rows: searchHashtag } = await postRepository.searchHashtag(body);
+    const { rows: findHashtag } = await postRepository.findHashtag(
+      searchHashtag
+    );
+    if (searchHashtag.length === 0) {
       await postRepository.createPost(body);
-      res.status(201).send({ ...body, userPhoto: verified.photo });
-      return;
+      return res.status(201).send({ ...body, userPhoto: verified.photo });
     }
-    await postRepository.insertHashtag(findHashtag.rows);
-    res.status(201).send({ ...body, userPhoto: verified.photo });
+
+    if (findHashtag.length === 0) {
+      const { rows: hashtagId } = await postRepository.insertHashtag(
+        searchHashtag[0].hashtag
+      );
+      const { rows: idPost } = await postRepository.createPost(body);
+      await postRepository.postHashtag(hashtagId[0].id, idPost[0].id);
+      return res.status(201).send({ ...body, userPhoto: verified.photo });
+    }
+
+    const { rows: idPost } = await postRepository.createPost(body);
+    await postRepository.postHashtag(findHashtag[0].id, idPost[0].id);
+    return res.status(201).send({ ...body, userPhoto: verified.photo });
+
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -4,28 +4,40 @@ import dayjs from "dayjs";
 function createPost(body) {
   const now = dayjs();
   return connection.query(
-    `INSERT INTO posts ("userId", url, description, "createdAt") VALUES ($1::int, $2, $3, $4) `,
+    `INSERT INTO posts ("userId", url, description, "createdAt") VALUES ($1, $2, $3, $4)
+    RETURNING id`,
     [body.userId, body.url, body.description, now]
   );
 }
 
 function searchHashtag(body) {
   return connection.query(
-    `SELECT id AS "postId", REGEXP_MATCHES('$1', '#([A-Za-z0-9_]+)', 'g') AS hashtag FROM posts WHERE posts.description = $2`,
+    `SELECT id AS "postId", REGEXP_MATCHES($1, '#([A-Za-z0-9_]+)', 'g') AS hashtag FROM posts WHERE posts.description = $2`,
     [body.description, body.description]
   );
 }
 
 function findHashtag(hashtag) {
   return connection.query(`SELECT * FROM hashtags WHERE name = $1`, [
-    hashtag[0].hashtag,
+    hashtag[0].hashtag[0],
   ]);
 }
 
 function insertHashtag(hashtag) {
-  return connection.query(`INSERT INTO hashtags (name) VALUES ($1)`, [
-    hashtag[0].name,
+  return connection.query(`INSERT INTO hashtags (name) VALUES ($1) RETURNING id`, [
+    hashtag[0],
   ]);
 }
-
-export default { createPost, searchHashtag, findHashtag, insertHashtag };
+function postHashtag(hashtagId, postId) {
+  return connection.query(
+    `INSERT INTO "hashtagsPost" ("hashtagId", "postId") VALUES ($1, $2)`,
+    [hashtagId, postId]
+  );
+}
+export default {
+  createPost,
+  searchHashtag,
+  findHashtag,
+  insertHashtag,
+  postHashtag,
+};
