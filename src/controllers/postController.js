5c52c8fa446bc6a9a466748e90d1f885ed6ce2df
thaisealves <alves.thaisesilva@gmail.com>
2022-08-15 15:31:02 -0300

    feat: adding the update post route

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -7,7 +7,7 @@ export async function newPost(req, res) {
   };
   try {
     const { rows: searchHashtag } = await postRepository.searchHashtag(body);
-    
+
     if (searchHashtag.length === 0) {
       await postRepository.createPost(body);
       return res.status(201).send({ ...body, userPhoto: verified.photo });
@@ -29,6 +29,17 @@ export async function newPost(req, res) {
     const { rows: idPost } = await postRepository.createPost(body);
     await postRepository.postHashtag(findHashtag[0].id, idPost[0].id);
     return res.status(201).send({ ...body, userPhoto: verified.photo });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function updatePost(req, res) {
+  const { description, postId } = req.body;
+  try {
+    await postRepository.updatePost(description, postId);
+    return res.status(200).send(...req.body);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
