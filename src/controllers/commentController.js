9c094d7b2843b0719d0427ccdf1f7b9262f81ac5
thaisealves <alves.thaisesilva@gmail.com>
2022-08-18 23:02:33 -0300

    fix: sending a status code on posting comment

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,6 +3,7 @@ import commentRepository from "../repositories/commentRepository.js";
 export async function postComment(req, res) {
   try {
     await commentRepository.postComment(req.body);
+    res.sendStatus(201);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
