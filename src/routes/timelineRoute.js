be27be60cc79f837cb010effa8c1dd3a874b6805
thaisealves <alves.thaisesilva@gmail.com>
2022-08-19 12:01:15 -0300

    fix: bug fixing from validateToken

--- src/routes/timelineRoute.js
+++ src/routes/timelineRoute.js
@@ -1,13 +1,13 @@
-import { Router } from 'express';
-import { validateToken } from '../middlewares/validateToken.js';
-import { getTimeline } from '../controllers/timelineController.js';
-import { getTrending } from '../controllers/trendingController.js';
-import { getPostByHashtag } from '../controllers/HashtagsController.js';
+import { Router } from "express";
+import { validateToken } from "../middlewares/validateToken.js";
+import { getTimeline } from "../controllers/timelineController.js";
+import { getTrending } from "../controllers/trendingController.js";
+import { getPostByHashtag } from "../controllers/HashtagsController.js";
 
 const router = Router();
 
-router.get('/timeline', getTimeline);
-router.get('/trending', getTrending);
-router.get('/hashtag/:hashtag', getPostByHashtag);
+router.get("/timeline", validateToken, getTimeline);
+router.get("/trending", getTrending);
+router.get("/hashtag/:hashtag", validateToken, getPostByHashtag);
 
 export default router;
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,19 +1,17 @@
-import express from 'express';
-import { userSignup } from '../controllers/signupController.js';
-import { userSignin } from '../controllers/signinController.js';
-import { getUserById, followUserById } from '../controllers/userController.js';
-import { validadeSignup } from '../middlewares/validateSignup.js';
-import { validateSignin } from '../middlewares/validateSignin.js';
-import { getUsers } from '../controllers/usersController.js';
-import { validateToken } from "../middlewares/validateToken.js";
-import { validadeUserId } from "../middlewares/validateUserId.js"
+import express from "express";
+import { userSignup } from "../controllers/signupController.js";
+import { userSignin } from "../controllers/signinController.js";
+import { getUserById, followUserById } from "../controllers/userController.js";
+import { validadeSignup } from "../middlewares/validateSignup.js";
+import { validateSignin } from "../middlewares/validateSignin.js";
+import { getUsers } from "../controllers/usersController.js";
+import { validadeUserId } from "../middlewares/validateUserId.js";
 const router = express.Router();
 
-
-router.post('/signup', validadeSignup, userSignup);
-router.post('/signin', validateSignin, userSignin);
+router.post("/signup", validadeSignup, userSignup);
+router.post("/signin", validateSignin, userSignin);
 router.get("/user/:id", getUserById);
-router.get('/users', validateToken, getUsers);
-router.post("/follow/:id", validadeUserId, followUserById)
+router.get("/users", getUsers);
+router.post("/follow/:id", validadeUserId, followUserById);
 
 export default router;
