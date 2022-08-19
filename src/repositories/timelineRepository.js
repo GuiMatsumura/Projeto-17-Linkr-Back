9ed0905fb1a68af153cfcd0a25b8709fa7fc626c
thaisealves <alves.thaisesilva@gmail.com>
2022-08-19 12:53:45 -0300

    feat: getting the query for reposts

--- src/repositories/timelineRepository.js
+++ src/repositories/timelineRepository.js
@@ -2,14 +2,51 @@ import connection from "../dbStrategy/postgres.js";
 
 async function getPosts() {
   return connection.query(
-    `SELECT posts.id, users.photo AS photo, users.username AS name, users.id AS "userId", posts.description, posts.url, metadata.title AS "metadataTitle", 
-    metadata.description AS "metadataDescription", metadata.img AS "metadataImg", COUNT(comments."postId") AS "numberOfComments"
-    FROM posts JOIN users ON users.id = posts."userId" 
+    `SELECT 
+    users.id as "userId",
+    users.username AS name,
+    users."photo" as photo, 
+    posts.description,
+    metadata.title AS "metadataTitle",
+    metadata.description AS "metadataDescription", 
+    metadata.img AS "metadataImg", 
+    COUNT(comments."postId") AS "numberOfComments",
+    posts.url,
+    posts.id as "postId",
+    NULL as "repostedByName",
+    NULL as "repostedBy",
+    posts."createdAt"
+    FROM posts
+    LEFT JOIN users ON users.id=posts."userId"
     JOIN metadata ON posts.id = metadata."postId"
     LEFT JOIN comments on posts.id = comments."postId"
-    GROUP BY posts.id, users.photo, users.username, users.id, metadata.title,  metadata.description, metadata.img
-    ORDER BY posts."createdAt" 
-    DESC LIMIT 20`
+    GROUP BY posts.id, users.id, metadata.title, metadata.description, metadata.img
+
+    UNION
+    
+    SELECT 
+    u1.id as "userId",
+    u1.username AS name,
+    u1."photo" as photo, 
+    posts.description,
+    metadata.title AS "metadataTitle",
+    metadata.description AS "metadataDescription", 
+    metadata.img AS "metadataImg", 
+    COUNT(comments."postId") AS "numberOfComments",
+    posts.url,
+    posts.id as "postId",
+    "reposterUser".username as "repostedByName",
+    "reposterUser".id as "repostedBy",
+    reposts."createdAt"
+    FROM reposts
+    LEFT JOIN posts ON reposts."postId"=posts.id
+    LEFT JOIN users u1 ON u1.id=posts."userId"
+    LEFT JOIN users "reposterUser" ON "reposterUser".id=reposts."userId"
+    JOIN metadata ON posts.id = metadata."postId"
+    LEFT JOIN comments on posts.id = comments."postId"
+    GROUP BY posts.id, u1.id, reposts."createdAt", reposts."userId", "reposterUser".username, "reposterUser".id,metadata.title, metadata.description, metadata.img
+    ORDER BY "createdAt" DESC
+   LIMIT 20`
   );
 }
 
